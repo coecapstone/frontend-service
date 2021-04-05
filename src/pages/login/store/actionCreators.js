2e376aea9d96a886448c949f8a6ca058e03d5a09
Yusie <xiyueyao@uw.edu>
2021-04-04 20:14:31 -0700

    test PR

--- src/pages/login/store/actionCreators.js
+++ src/pages/login/store/actionCreators.js
@@ -9,71 +9,73 @@ export const CHANGE_ROLE_TO_APPROVER = 'login/CHANGE_ROLE_TO_APPROVER';
 export const CHANGE_ROLE_TO_SUBMITTER = 'login/CHANGE_ROLE_TO_SUBMITTER';
 
 const changeLogin = (profile) => ({
-    type: CHANGE_TO_LOGIN,
-    profile: fromJS(profile)
-})
+	ddd: 'ddd',
+	type: CHANGE_TO_LOGIN,
+	profile: fromJS(profile)
+});
 
 const changeApprovalSubunitList = (approvalSubunitList) => ({
-    type: CHANGE_APPROVAL_SUBUNIT_LIST,
-    approvalSubunitList: fromJS(approvalSubunitList)
-})
+	type: CHANGE_APPROVAL_SUBUNIT_LIST,
+	approvalSubunitList: fromJS(approvalSubunitList)
+});
 
 export const logout = () => ({
-    type: CHANGE_TO_LOGOUT
-})
+	type: CHANGE_TO_LOGOUT
+});
 
 export const changeApprovalInfo = (unitSubunitInfo) => {
-    const split = unitSubunitInfo.split('@');
-    return {
-        type: CHANGE_APPROVAL_INFO,
-        unit: split[1], 
-        subunit: split[0]
-    }
-}
+	const split = unitSubunitInfo.split('@');
+	return {
+		type: CHANGE_APPROVAL_INFO,
+		unit: split[1],
+		subunit: split[0]
+	};
+};
 
 export const changeRoleToSubmitter = () => ({
-    type: CHANGE_ROLE_TO_SUBMITTER
-})
+	type: CHANGE_ROLE_TO_SUBMITTER
+});
 
 export const changeRoleToApprover = () => ({
-    type: CHANGE_ROLE_TO_APPROVER
-})
+	type: CHANGE_ROLE_TO_APPROVER
+});
 
 export const login = (profile) => {
-    return (dispatch) => {
-        const netId = profile.email.split('@')[0];
-        const url = `http://localhost:8080/api/getUserRole/${netId}`;
-        console.log(url)
-        console.log('url')
-        axios.get(url)
-            .then(res => {
-                console.log(res.data);
-                const approvalSubunitList = [];
-                const data = res.data;
-                for(const val in data) {
-                    const unit = data[val].unitName;
-                    const subUnit = data[val].subUnitName;
-                    const text = `${unit}, ${subUnit}`;
-                    const subunit = {};
-                    subunit.key = subUnit;
-                    subunit.text = text;
-                    subunit.value = `${subUnit}@${unit}`;
-                    approvalSubunitList.push(subunit);
-                }
-                console.log(approvalSubunitList);
-                if (approvalSubunitList.length > 0) {
-                    dispatch(changeRoleToApprover());
-                }
-                dispatch(changeApprovalSubunitList(approvalSubunitList));
-                dispatch(changeLogin(profile));
-                // save to session story?
-                //dispatch(submitFormAction())
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
-}
+	return (dispatch) => {
+		const netId = profile.email.split('@')[0];
+		const url = `http://localhost:8080/api/getUserRole/${netId}`;
+		console.log(url);
+		console.log('url');
+		axios
+			.get(url)
+			.then((res) => {
+				console.log(res.data);
+				const approvalSubunitList = [];
+				const data = res.data;
+				for (const val in data) {
+					const unit = data[val].unitName;
+					const subUnit = data[val].subUnitName;
+					const text = `${unit}, ${subUnit}`;
+					const subunit = {};
+					subunit.key = subUnit;
+					subunit.text = text;
+					subunit.value = `${subUnit}@${unit}`;
+					approvalSubunitList.push(subunit);
+				}
+				console.log(approvalSubunitList);
+				if (approvalSubunitList.length > 0) {
+					dispatch(changeRoleToApprover());
+				}
+				dispatch(changeApprovalSubunitList(approvalSubunitList));
+				dispatch(changeLogin(profile));
+				// save to session story?
+				//dispatch(submitFormAction())
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
+};
 
 // const getFormListAction = (data) => ({
 //     type: GET_FORMLIST,
